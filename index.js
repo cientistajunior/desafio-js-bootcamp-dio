eae0bf484b5b0d7df9e5bad11e44f04f7d64643f
Wilson Jr <106275510+cientistajunior@users.noreply.github.com>
2023-11-10 15:40:41 -0300

    add inde.js
    adicionando codigo do desafio

---
+++ index.js
@@ -0,0 +1,20 @@
+let quantidade = "";
+let xp = 9007;
+
+if (xp < 1000) {
+  console.log("O Herói de nome Ferro está no nível de  1");
+} else if (xp >= 1001 && xp <= 2000) {
+  console.log("O Herói de nome Bronze está no nível de  2");
+} else if (xp >= 2001 && xp <= 5000) {
+  console.log("O Herói de nome Prata está no nível de  3");
+} else if (xp >= 6001 && xp <= 7000) {
+  console.log("O Herói de nome Ouro está no nível de  4");
+} else if (xp >= 7001 && xp <= 8000) {
+  console.log("O Herói de nome Platina está no nível de  5");
+} else if (xp >= 8001 && xp <= 9000) {
+  console.log("O Herói de nome Ascendente está no nível de  6");
+} else if (xp >= 9001 && xp <= 10000) {
+  console.log("O Herói de nome Imortal está no nível de  7");
+} else if (xp >= 10001) {
+  console.log("O Herói de nome Radiante está no nível de  8");
+}
